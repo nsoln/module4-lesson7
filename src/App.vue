1a711c07591ba5b0a34245587d88711ab9450c31
Nikolay Solnyshkin <nikolay.solnyshkin@list.ru>
2023-10-13 10:02:10 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box">
+    <div class="square"></div>
+  </div>
+</template>
+
+<style>
+.box{
+  display: flex;
+  justify-content: center;
+  
+}
+.square{
+  width: 40px;
+  height: 40px;
+  background-color: red;
+}
+
+</style>
